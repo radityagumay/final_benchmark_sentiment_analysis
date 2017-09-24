adce1caf17f2f80f769b6c38952a33acbf09e747
radityagumay <raditya.g@go-jek.com>
2017-09-24 21:14:00 +0530

    Remove unused class

--- com/radityalabs/sentiment/Sentiment.py
+++ com/radityalabs/sentiment/Sentiment.py
@@ -1,8 +1,15 @@
+import os
 
 class Sentiment:
-    def __init__(self, message):
-        print("Hello World " + message)
+    def __init__(self):
+        self.path = os.path.expanduser("~/PycharmProjects/FinalSentimentAnalysis/com/radityalabs/sentiment/data")
 
-sentiment = Sentiment
-sentiment("Raditya")
+    def load_review_collection(self):
+        with open(self.path + "/review_collection.txt", "r", encoding="utf-8") as f:
+            read = f.read().splitlines()
+            for row in read:
+                print(row)
 
+
+sentiment = Sentiment()
+sentiment.load_review_collection()
